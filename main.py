57341f1186a12bf408a53929f46024dd6693d164
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-11-10 09:22:51 +0530

    feat: pygame initial setup
    setup constants and initiate pygame

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame as pg
+
+from constants import *
+
+def main():
+    pg.init()
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        screen.fill("black")
+        pg.display.flip()
+
+
+if __name__ == "__main__":
+    main()
