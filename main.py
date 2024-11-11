5e315229cb66133a838e0f4c3f9da6afac25394d
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-11-11 18:03:32 +0530

    perf: limit the frame generation
    Decouple the game speed from the game rendering speed by limiting frame
    generation. This results in the cpu resource usage being limited.

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import pygame as pg
 
 from constants import *
 
+
 def main():
     pg.init()
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pg.time.Clock()
+    dt = 0
 
     while True:
         for event in pg.event.get():
@@ -13,6 +16,9 @@ def main():
 
         screen.fill("black")
         pg.display.flip()
+
+        # limit the frame rate to 60fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
