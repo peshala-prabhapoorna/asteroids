f8afc1765c06333ee4aa8031574fcfa64bd91900
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-11-15 01:09:17 +0530

    feat: add splitting behaviour to asteroids
    Depending on the size of the asteroid, when shot at will split into
    multiple small asteroids.

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.is_colliding(shot):
-                    asteroid.kill()
                     shot.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
